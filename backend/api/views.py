056954eeec20f30fab48c4c714a3c9018c950e62
Shubham <shubhambajaj204@gmail.com>
2023-03-12 19:02:20 +0530

    Understanding request bodya and parametes

--- backend/api/views.py
+++ backend/api/views.py
@@ -1,4 +1,21 @@
+import json
 from django.http import JsonResponse
 
 def api_home(request,*args, **kwargs):
+    #request ->HttpRequest -> Django
+    #print(dir(request))
+    #request.body
+    body = request.body #byte string of JSON Data
+    data={}
+    try:
+        data = json.loads(body) #String of json data ->python dictionary
+    except:
+        pass
+
+    print(data)
+    print(data.keys())
+    # data['headers']=request.headers #request.META -> 
+    print(request.headers)
+    data['content_type']=request.content_type
+    print(data)
     return JsonResponse({'message':'Hi there, this is your django api response!!!!'})
--- py_client/basic.py
+++ py_client/basic.py
@@ -1,15 +1,16 @@
 import requests
 # endpoint ="http://httpbin.org/status/200/"
-endpoint = "http://httpbin.org/anything"
-endpoint  ="http://localhost:7000/api"
+# endpoint = "http://httpbin.org/anything"
+endpoint  ="http://localhost:7000/api/"
 
-get_response = requests.get(endpoint,json={'query':'hello world'}) #HTTP Request
+get_response = requests.get(endpoint,params={'abc':123},json={'query':'hello world'}) #HTTP Request
 #form data json data
-print(get_response.text)#print raw text response
+# print(get_response.text)#print raw text response
+# print(get_response.status_code)
 
 #REST API -> Web API 
 
 # Javascript Object Notation ~python dict
 
-print(get_response.json()['message'])
-print(get_response.status_code)
+print(get_response.json())
+# print(get_response.status_code)
