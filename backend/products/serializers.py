5e6efe410641edc5813721011ffd4bcf721ae2e6
Shubham <shubhambajaj204@gmail.com>
2023-03-13 15:50:52 +0530

    person app created

--- backend/products/serializers.py
+++ backend/products/serializers.py
@@ -3,7 +3,7 @@ from rest_framework import serializers
 from .models import Product
 
 class ProductSerializer(serializers.ModelSerializer):
-    discount  = serializers.SerializerMethodField(read_only=True)
+    # discount  = serializers.SerializerMethodField(read_only=True)
     class Meta:
         model = Product
         fields=[
